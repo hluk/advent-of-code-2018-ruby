d953df01c829c6009e53c7e65db1fdf1a5e55316
Lukas Holecek <hluk@email.cz>
2018-12-05 18:57:11 +0100

    Day 5: Clean up
    Signed-off-by: Lukas Holecek <hluk@email.cz>

--- 05/main.rb
+++ 05/main.rb
@@ -1,51 +1,20 @@
 #!/usr/bin/env ruby
-require 'set'
-
-def trace_polymer(polymer, i, emph)
-  return
-  from = [i - 10, 0].max
-  if emph
-    print "#{polymer[from...i - 1]}_#{polymer[i - 1..i]}_#{polymer[i + 1..i + 10]}\n"
-  else
-    puts polymer[from..i + 10]
-  end
-end
-
-def reduce_step(polymer, i)
-  trace_polymer polymer, i, true
-  polymer[i - 1..i] = ''
-  trace_polymer polymer, i, false
-  i -= 1
-  if i < 0
-    i = 1
-  end
-  i
-end
-
 def reduce_polymer(polymer)
-  i = 1
-  while i < polymer.length
-    if polymer[i - 1].swapcase == polymer[i]
-      i = reduce_step(polymer, i)
-    else
-      i += 1
-    end
+  (polymer.length - 2).downto(0) do |i|
+    polymer[i..i + 1] = '' if polymer[i] == polymer[i + 1].swapcase
   end
 
   polymer.length
 end
 
-input_file = ARGV.fetch(0)
-polymer = File.read(input_file).strip
+def min_length(polymer)
+  ('a'..'z')
+    .map { |letter| polymer.tr("#{letter}#{letter.upcase}", '') }
+    .map(&method(:reduce_polymer))
+    .min
+end
+
+polymer = ARGF.read.chomp
 
 puts reduce_polymer(polymer)
-
-letters = Set.new(polymer.downcase.chars)
-lengths = letters.map do |letter|
-  polymer1 = polymer.clone
-  polymer1.gsub!(letter.downcase, '')
-  polymer1.gsub!(letter.upcase, '')
-  reduce_polymer(polymer1)
-  polymer1.length
-end
-puts lengths.min
+puts min_length(polymer)
